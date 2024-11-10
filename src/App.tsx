daa7c7467dc02dda4e5e8e03716dbb7e5b97feb1
toshi119 <kakakatoshi119@gmail.com>
2024-11-10 20:10:35 +0900

    define type CartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Wrapper } from "./App.styles";
+import { CartItemType } from "./types";
+
+const getProducts = async () => {
+  await (await fetch('https://fakestoreapi.com/products')).json();
+};
+
 const App = () => {
   return (
     <div className="App">
